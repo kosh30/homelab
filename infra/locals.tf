ce4909f04f53e0316c54ec63c17221419dde4f53
Wjatscheslaw Poluschin (Kosh) <wjatscheslaw@poluschin.com>
2025-06-18 13:21:08 +0200

    fix(infra): align spacing in locals.tf for readability

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,14 +1,14 @@
 locals {
-  proxmox_config        = yamldecode(data.sops_file.global_secret.raw).proxmox
-  gitlab_config         = yamldecode(data.sops_file.global_secret.raw).gitlab
-  talos_version         = var.talos_version
-  talos_upgrade_version = var.talos_upgrade_version != "" ? var.talos_upgrade_version : var.talos_version
-  proxmox_network_cidr  = nonsensitive(split("/", local.proxmox_config.network)[1])
-  controller_network    = nonsensitive(cidrsubnet(local.proxmox_config.network, 6, 15 + 1))
-  worker_network        = nonsensitive(cidrsubnet(local.proxmox_config.network, 6, 15 + 2))
-  mac_prefix = "BC:24:11:D1"
-  controlplane_mac_suffix ="61"
-  worker_mac_suffix ="64"
+  proxmox_config          = yamldecode(data.sops_file.global_secret.raw).proxmox
+  gitlab_config           = yamldecode(data.sops_file.global_secret.raw).gitlab
+  talos_version           = var.talos_version
+  talos_upgrade_version   = var.talos_upgrade_version != "" ? var.talos_upgrade_version : var.talos_version
+  proxmox_network_cidr    = nonsensitive(split("/", local.proxmox_config.network)[1])
+  controller_network      = nonsensitive(cidrsubnet(local.proxmox_config.network, 6, 15 + 1))
+  worker_network          = nonsensitive(cidrsubnet(local.proxmox_config.network, 6, 15 + 2))
+  mac_prefix              = "BC:24:11:D1"
+  controlplane_mac_suffix = "61"
+  worker_mac_suffix       = "64"
 
   controllers = {
     "ctrl-001" = {
