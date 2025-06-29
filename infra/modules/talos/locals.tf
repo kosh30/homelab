0e9ad5239e3b7b776e8bce437ae7320bcad1a9f7
Wjatscheslaw Poluschin (Kosh) <wjatscheslaw@poluschin.com>
2025-06-29 22:56:43 +0200

    chore(talos): update worker labels with platform-specific node label

--- infra/modules/talos/locals.tf
+++ infra/modules/talos/locals.tf
@@ -2,5 +2,5 @@ locals {
   proxmox_network_cidr = split("/", var.cluster.proxmox_network)[1]
   controller_network   = cidrsubnet(var.cluster.proxmox_network, 6, 15 + 1)
   worker_network       = cidrsubnet(var.cluster.proxmox_network, 6, 15 + 2)
-  worker_labels        = "node-pool=worker"
+  worker_labels        = "node-pool=worker,node-labels: node.cloudprovider.kubernetes.io/platform=nocloud"
 }
